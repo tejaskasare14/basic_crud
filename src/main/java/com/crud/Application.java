5f01f56ec895cc7527403226d1ab5da6a4df4bdd
tejaskasare14 <tejaskasare.projects@gmail.com>
2023-09-20 20:10:16 +0530

    first print removed

--- src/main/java/com/crud/Application.java
+++ src/main/java/com/crud/Application.java
@@ -10,7 +10,7 @@ public class Application {
 	public static void main(String[] args) {
 
 		SpringApplication.run(Application.class, args);
-		System.out.println("hello spring");
+		
 		System.out.println("hello spring");
 	}
 	
